7544564ecc9880c75503c89b0664e68de074f3d9
Ankit <ankit@cypress.io>
2022-10-24 11:49:43 -0400

    added slack notifications for erroring frameworks

--- script.mjs
+++ script.mjs
@@ -1,21 +1,23 @@
 import * as ncu from "npm-check-updates";
+import axios from 'axios';
 import * as path from "path";
 import { execa } from "execa";
 import crypto from "crypto";
 import * as fs from "fs/promises";
 import * as dotenv from 'dotenv'
+import { type } from "os";
 dotenv.config()
 
 /** @type {Array<{ projectName: string, filters: string[], tag: string }>} */
 const projects = [
-  { projectName: "angular", filters: ["@angular*"], tag: "@angular/core" },
+  // { projectName: "angular", filters: ["@angular*"], tag: "@angular/core" },
   // { projectName: "react-cra5-ts", filters: ["react-scripts"], tag: 'react-scripts' },
   // { projectName: 'react-next12-ts', filters: ['next'], tag: 'next' },
   // { projectName: 'react-vite-ts', filters: ['vite', '@vitejs/plugin-react'], tag: 'vite' },
   // { projectName: 'react-webpack5-js', filters: ['react', 'react-dom'], tag: 'react' },
   // { projectName: 'svelte-vite-ts', filters: ['svelte', '@sveltejs/vite-plugin-svelte', 'svelte-preprocess'], tag: 'svelte' },
   // { projectName: 'vue3-cli5-ts', filters: ['@vue/cli*'], tag: '@vue/cli-service' },
-  // { projectName: 'vue2-nuxt2-js', filters: ['nuxt', '@nuxtjs*'], tag: 'nuxt' }
+  { projectName: 'vue2-nuxt2-js', filters: ['nuxt', '@nuxtjs*'], tag: 'nuxt' }
 ];
 
 function createHash(input) {
@@ -66,6 +68,35 @@ async function getFailedRuns() {
 
 async function saveFailedRuns(content) {
   await fs.writeFile("./project-fails.json", JSON.stringify(content, null, 2));
+}
+
+async function slackErrors () {
+  const newProjectFails = await getFailedRuns();
+  if (newProjectFails.length === 0) return;
+  let content = {
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "The following Nightly builds failed:"
+        }
+      },
+    ]
+  }
+  let iterator = 2;
+  // figure out the dividers
+  for (const [projectName, value]  of Object.entries(newProjectFails)) {
+    content['blocks'].push({"type": "divider"})
+    content['blocks'].push({ "type": "section", "fields": []})
+    content['blocks'][iterator]['fields'].push({ "type": "mrkdwn", "text": `*Project Name:*\n ${projectName}`})
+    content['blocks'][iterator]['fields'].push({ "type": "mrkdwn", "text": `*Tag:*\n ${value['tag']}`})
+    content['blocks'][iterator]['fields'].push({ "type": "mrkdwn", "text": `*Error:*\n ${JSON.stringify(value['error'])}`})
+    iterator += 2;
+  }
+
+  axios.post('https://hooks.slack.com/services/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', content)
+  
 }
 
 const cwd = process.cwd();
@@ -152,7 +183,6 @@ async function run(projectHashes, projectFails, { projectName, filters, tag }) {
       projectFails[`${projectName}-${target}`]['error'] = e;
 
       await saveFailedRuns(projectFails)
-    }
 
     if (!projectHashes[projectName]) {
       projectHashes[projectName] = {};
@@ -163,6 +193,7 @@ async function run(projectHashes, projectFails, { projectName, filters, tag }) {
     await updateProjectHashes(projectHashes);
 
     await restorePackageJson(projectName);
+    }
   }
 }
 
@@ -174,17 +205,11 @@ async function main() {
   for (const project of projects) {
     await run(projectHashes, projectFails, project);
   }
-}
+  slackErrors()
+};
 
 main();
 
 /**
- * Hash Schema
- * {
- * FrameworkName: {newest: hashValue, latest: hashValue}
- * }
- */
-/**
- * For all failed runs:
- * FrameworkName-target: {tag: tagValue , error: errorValue}
+ * 
  */
